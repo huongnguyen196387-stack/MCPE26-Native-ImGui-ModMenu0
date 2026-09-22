1d86c9efaa9d4e01d72cfff93070e87adee023c7
huongnguyen196387-stack <huongnguyen196387@gmail.com>
2026-09-22 10:33:47 +0700

    Update native_bridge.cpp

--- app/src/main/cpp/native_bridge.cpp
+++ app/src/main/cpp/native_bridge.cpp
@@ -2,23 +2,28 @@
 #include <jni.h>
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_example_mcpeimgui_MainActivity_nativeInit(JNIEnv*, jclass) {
+Java_com_example_mcpeimgui_MainActivity_nativeInit(
+        JNIEnv*,
+        jclass,
+        jint width,
+        jint height) {
     modmenu::Init();
-}
-
-extern "C" JNIEXPORT void JNICALL
-Java_com_example_mcpeimgui_MainActivity_nativeResize(
-    JNIEnv*, jclass, jint width, jint height) {
     modmenu::Resize(width, height);
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_example_mcpeimgui_MainActivity_nativeRender(JNIEnv*, jclass) {
+Java_com_example_mcpeimgui_MainActivity_nativeRender(
+        JNIEnv*,
+        jclass) {
     modmenu::Render();
 }
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_example_mcpeimgui_MainActivity_nativeTouch(
-    JNIEnv*, jclass, jint action, jfloat x, jfloat y) {
+        JNIEnv*,
+        jclass,
+        jint action,
+        jfloat x,
+        jfloat y) {
     modmenu::Touch(action, x, y);
 }
